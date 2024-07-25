ffe534b8f9a24f1eae147b69400015c1dcd14380
Mattea <mattea.sax@gmail.com>
2024-07-25 16:35:03 +0200

    installed cors in backend and added API url to env

--- src/services/UserAPI.ts
+++ src/services/UserAPI.ts
@@ -3,8 +3,17 @@
  *
  */
 
+import axios from "axios";
+
 const API_URL = import.meta.env.VITE_API_URL;
 
 /**
  * Create a new user
  */
+
+export const registerUser = (userData: {
+  username: string;
+  password: string;
+}) => {
+  return axios.post(`${API_URL}/register`, userData);
+};
